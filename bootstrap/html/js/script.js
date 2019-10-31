9a27a77047909f7f596c0c8ea8809380d38c78a1
vitShum <https://github.com/vitShum>
2019-10-31 10:21:01 +1000

    TyTБылVanek

---
+++ bootstrap/html/js/script.js
@@ -0,0 +1,6 @@
+// $('#carouselID').carousel({
+//     interval: 2000
+// })
+// $('#exampleModal').modal({
+//     backdrop: "static"
+// })
---
+++ bootstrap/php/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="gamestyle.css">
+    <link rel="shortcut icon" href="../bootstrap/pictures/gameLoGopng.png" type="image/x-icon">
+    <title>GamesStyle</title>
+  </head>
+  <body>
+      <nav class="navbar navbar-expand-lg navbar-dark sticky-top" style="background-color: #4c0583;">
+          <a href="#" class="navbar-brand">
+              <img src="../bootstrap/pictures/gameLoGopng.png" width="30" height="30" alt="logo">
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <a href="#" class="nav-link">Home</a>
+                  </li>
+                <div class="dropdown">
+                  <li class="nav-item">
+                    <a href="#" class="nav-link dropdown-toggle" role="button" id="dropdownMenuLink" 
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Games</a>
+                    <div class="dropdown-menu" style="background-color: blueviolet;"ria-labelledby="dropdownMenuLink">
+                      <a class="dropdown-item" href="phpstyle.php">Game</a>
+                      <a class="dropdown-item" href="#">Production</a>
+                      <a class="dropdown-item" href="#">Game Device</a>
+                    </div>
+                  </li>
+                </div>
+                <div class="dropdown">
+                  <li class="nav-item">
+                    <a href="#" class="nav-link dropdown-toggle" role="button" id="dropdownMenuLink" 
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Selling</a>
+                    <div class="dropdown-menu" style="background-color: blueviolet;" aria-labelledby="dropdownMenuLink">
+                      <a class="dropdown-item" href="#">Sell</a>
+                      <a class="dropdown-item" href="#">Selling</a>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </li>
+                </div>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link" data-toggle="modal" data-target="#exampleModal">Blog</a>
+                  </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                  <input type="text" class="form-control mr-sm-2" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-danger my-2 my-sm-0">Search</button>
+              </form>
+          </div>
+      </nav>
+
+      <footer>
+    <div class="footer">#Code 2019</div>
+  </footer>
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<div class="container"></div>
